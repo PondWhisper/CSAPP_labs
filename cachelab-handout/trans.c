1b2a0af020f7214689daae22925e616af5ad015d
Jingxici <3820228386@qq.com>
2026-05-01 18:18:30 +0800

    Complete the cachelab PartB.

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -35,15 +35,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+    
 }
 
 /*
